f28a517e32935f92917f1b442db4502accdf2ab6
nicolascossi <77031613+nicolascossi@users.noreply.github.com>
2023-01-26 18:11:48 -0300

    Update index.js

--- proyecto js/index.js
+++ proyecto js/index.js
@@ -1,3 +1,4 @@
+
 let product1 = "Tablet";
 let price1 = 200;
 let product2 = "Laptop";
@@ -8,22 +9,26 @@ let price3 = 500;
 let total = 0;
 let continueShopping = true;
 
+function sumar(total, price) {
+    return total + price;
+}
+
 while (continueShopping) {
-  let userChoice = prompt("Selecciona un producto: 1. " + product1 + " - $" + price1 + " 2. " + product2 + " - $" + price2 + " 3. " + product3 + " - $" + price3);
+  let userChoice = prompt("Seleccione un producto: 1. " + product1 + " - $" + price1 + " 2. " + product2 + " - $" + price2 + " 3. " + product3 + " - $" + price3);
 
   if (userChoice == 1) {
-    total += price1;
-    console.log("You have selected: " + product1 + " - $" + price1);
+    total = sumar(total, price1);
+    console.log("Tu has elegido: " + product1 + " - $" + price1);
   } else if (userChoice == 2) {
-    total += price2;
-    console.log("You have selected: " + product2 + " - $" + price2);
+    total = sumar(total, price2);
+    console.log("Tu has elegido: " + product2 + " - $" + price2);
   } else if (userChoice == 3) {
-    total += price3;
-    console.log("You have selected: " + product3 + " - $" + price3);
+    total = sumar(total, price3);
+    console.log("Tu has elegido: " + product3 + " - $" + price3);
   } else {
     console.log("Invalido. Por favor, elija un número entre 1 y 3.");
   }
-  console.log("Current total: $" + total);
+  console.log("Actual total: $" + total);
   let keepShopping = prompt("Queres continuar comprando? Escribe 'yes' or 'no'").toLowerCase();
   if (keepShopping === "no") {
     continueShopping = false;
@@ -31,3 +36,8 @@ while (continueShopping) {
 }
 console.log("Total: $" + total);
 console.log("Gracias por comprar!");
+
+
+
+
+
